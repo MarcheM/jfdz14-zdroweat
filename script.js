0f2bf1134c9a3ebedb51cdb95b2d200ef12e0e45
Michał Marchewa <michalmarchewa@gmail.com>
2020-06-20 10:26:38 +0200

    smaller nav when scrolling

--- script.js
+++ script.js
@@ -100,6 +100,18 @@ document
   .querySelectorAll(bigDivSelector)
   .forEach((element) => intersectionObserver2.observe(element))
 
+//shrink nav on scroll
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector(".menu").style.height = "30px";
+
+  } else {
+    document.querySelector(".menu").style.height = "50px";
+  }
+
+})
+
 //=========================== Carousel =============================
 
 let index = 0;
